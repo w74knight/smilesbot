bae769fb5cb6b43fb56beee1ac6c6215d51505c4
Koy Pena Barraca <barraca.koy@gmail.com>
2025-05-11 23:59:47 -0400

    Missed splitting function: fixing issue with commas (`,`) and IUPAC names

--- src/smile/smile.py
+++ src/smile/smile.py
@@ -188,7 +188,7 @@ class Smile(object):
     async def render_molecule(self, ctx, molecule, server_id, legends, **drawFuncArgs) -> None:
         self.logger.info(f"smile.render_molecule(ctx, {molecule}, {server_id}, {legends})")
 
-        molecules = [m.strip() for m in molecule.split(",")]
+        molecules = [m.strip() for m in molecule.split(";")]
         mol_objects = []
 
         if len(molecules) > 4:
